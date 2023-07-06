c5384010f627377240572c223eae4dc588e0194a
unknown <robertgicheha3@gmail.com>
2023-07-06 10:13:27 +0300

    worked on folder structure,components,layout and configs

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-6xl" mb-4>
+        Welcome
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
