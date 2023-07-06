8f7229afa6685d97841f9bab548302901670cdb1
unknown <robertgicheha3@gmail.com>
2023-07-06 13:27:42 +0300

    worked on alert component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./Pages/About";
 import NotFound from "./Pages/NotFound";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Alert from "./components/layout/Alert";
 import { GithubProvider } from "./context/github/GithubContext";
 import { AlertProvider } from "./context/alert/AlertContext";
 
@@ -16,6 +17,8 @@ function App() {
             <Navbar />
 
             <main className="container mx-auto px-3 pb-12">
+              <Alert />
+
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
