b7c71883d77eb8c742dcee97463e74611a0cc2e9
unknown <robertgicheha3@gmail.com>
2023-07-06 12:44:45 +0300

    Worked on UserSearch Component and functionality

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,11 @@
 import UserResults from "../components/users/UserResults";
-
+import UserSearch from "../components/users/UserSearch";
 
 function Home() {
   return (
     <>
-    {/* Search Component */}
-     <UserResults />
+      <UserSearch />
+      <UserResults />
       {/* {process.env.REACT_APP_GITHUB_TOKEN} */}
     </>
   );
