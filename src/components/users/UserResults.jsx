2fc9f353990d349de1740fe915b84586ce083cb2
unknown <robertgicheha3@gmail.com>
2023-07-06 12:01:36 +0300

    worked Context API, that is Github Context

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,25 +1,28 @@
-import { useEffect, useState } from "react";
+import { useEffect, useContext } from "react";
 import Spinner from "../layout/Spinner";
 import UserItem from "../users/UserItem";
+import GithubContext from "../../context/github/GithubContext";
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  // const [users, setUsers] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
+
   useEffect(() => {
     fetchUsers();
-  }, []);
+  });
 
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-    const data = await response.json();
-    // console.log(data);
-    setUsers(data);
-    setLoading(false);
-  };
+  // const fetchUsers = async () => {
+  //   const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+  //     headers: {
+  //       Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+  //     },
+  //   });
+  //   const data = await response.json();
+  //   // console.log(data);
+  //   setUsers(data);
+  //   setLoading(false);
+  // };
 
   if (!loading) {
     return (
